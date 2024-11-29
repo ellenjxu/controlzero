f9544b037a77f5e8476b2568f04e921b854f98c1
Ellen Xu <ellen2imagine@gmail.com>
2024-11-28 23:29:03 -0800

    clean

--- run_a0c.py
+++ run_a0c.py
@@ -10,10 +10,12 @@ import gym_cartlataccel
 import matplotlib.pyplot as plt
 from torchrl.data import ReplayBuffer, LazyTensorStorage
 from tensordict import TensorDict
+
 from networks.mcts import MCTS, A0CModel
 from networks.agent import ActorCritic
-from run_mcts import CartState
+from utils.cartstate import CartState
 from utils.running_stats import RunningStats
+from utils.evaluate import sample_rollout, plot_losses
 
 import warnings
 warnings.filterwarnings("ignore", category=UserWarning)
@@ -136,7 +138,7 @@ class A0C:
         self.optimizer.step()
       
       # log metrics
-      avg_reward = np.mean(evaluate(self.model.actor, self.env))
+      avg_reward = np.mean(sample_rollout(self.model.actor, self.env, n_episodes=5, n_steps=200))
 
       self.hist['iter'].append(i)
       self.hist['reward'].append(avg_reward)
@@ -149,52 +151,6 @@ class A0C:
 
     print(f"Total time: {time.time() - self.start}")
     return self.model, self.hist
-
-def evaluate(model, env, n_episodes=10, n_steps=200, seed=42):
-  eps_rewards = []
-  for i in range(n_episodes):
-    rewards = rollout(model, env, max_steps=n_steps, seed=seed+i, deterministic=True)
-    eps_rewards.append(sum(rewards))
-  return eps_rewards
-
-def rollout(model, env, max_steps=1000, seed=42, deterministic=False, device='cpu'):
-  state, _ = env.reset(seed=seed)
-  rewards = []
-  for _ in range(max_steps):
-    state_tensor = torch.FloatTensor(state).unsqueeze(0).to(device)
-    action = model.get_action(state_tensor, deterministic=deterministic)
-    next_state, reward, terminated, truncated, info = env.step(action)
-    done = terminated or truncated
-    state = next_state
-    rewards.append(reward)
-    if done:
-      state, _ = env.reset()
-      break
-  return rewards
-
-def plot_losses(hist, save_path=None):
-  plt.figure(figsize=(10, 6))
-  # Create subplots for better visualization
-  fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 8))
-  # Plot losses
-  ax1.plot(hist['iter'], hist['total_loss'], label='Total Loss')
-  ax1.plot(hist['iter'], hist['value_loss'], label='Value Loss')
-  ax1.plot(hist['iter'], hist['policy_loss'], label='Policy Loss')
-  ax1.set_xlabel('n_iters')
-  ax1.set_ylabel('loss')
-  ax1.legend()
-  # Plot rewards
-  ax2.plot(hist['iter'], hist['reward'], label='Average Reward', color='green')
-  ax2.set_xlabel('n_iters')
-  ax2.set_ylabel('reward')
-  ax2.legend()
-  
-  plt.suptitle(f"A0C on CartLatAccel with {args.noise_mode} noise")
-  plt.tight_layout()
-  if save_path:
-    plt.savefig(save_path)
-  plt.show()
-  plt.close(fig)
 
 if __name__ == "__main__":
   parser = argparse.ArgumentParser()
@@ -223,7 +179,7 @@ if __name__ == "__main__":
   # run actor net model
   print("rollout out best actor")
   env = gym.make("CartLatAccel-v1", noise_mode=args.noise_mode, env_bs=1, render_mode="human")
-  rewards = evaluate(best_model.actor, env, n_episodes=10, n_steps=200)
+  rewards = sample_rollout(best_model.actor, env, n_episodes=10, n_steps=200)
   print(f"reward {np.mean(rewards):.3f}, std {np.std(rewards):.3f}")
 
   if args.save:
--- run_mcts.py
+++ run_mcts.py
@@ -1,56 +1,11 @@
-"""
-MCTS online planner on cartlataccel. Simulates n_sims for each state and chooses the best action
-"""
+""" MCTS online planner on cartlataccel. Simulates n_sims for each state and chooses the best action """
 
-import torch
 import argparse
 import numpy as np
 import gymnasium as gym
 import gym_cartlataccel
 from networks.mcts import MCTS, State
-
-class CartState(State):
-  def __init__(self, pos, vel, target):
-    self.pos = pos
-    self.vel = vel
-    self.target = target
-      
-  def __hash__(self):
-    return hash((self.pos, self.vel, self.target))
-  
-  def __eq__(self, other):
-    if not isinstance(other, CartState):
-      return False
-    return (self.pos == other.pos and self.vel == other.vel and self.target == other.target)
-
-  @classmethod
-  def from_array(cls, array):
-    return cls(array[0], array[1], array[2])
-  
-  def to_tensor(self, device='cpu'):
-    return torch.FloatTensor([self.pos, self.vel, self.target]).to(device)
-  
-  def generate(self, action):
-    # cartlataccel env.py
-    force_mag = 50.0
-    tau = 0.02
-    max_x = 10.0
-    max_episode_steps = 500
-    
-    action = np.clip(action, -1, 1)
-    new_a = action * force_mag
-    new_x = 0.5 * new_a * tau**2 + self.vel * tau + self.pos
-    new_x = np.clip(new_x, -max_x, max_x)
-    new_v = new_a * tau + self.vel
-    
-    next_state = CartState(new_x, new_v, self.target)
-    error = abs(new_x - self.target)
-    reward = -error/max_x
-    
-    return next_state, reward
-  
-  def sample_action(self):
-    return np.random.uniform(-1, 1) # max_u
+from utils.cartstate import CartState
 
 def run_mcts(mcts, env, max_steps, search_depth, n_sims, seed=42, deterministic=False):
   state, _ = env.reset(seed=seed)
